2015ba8ff21db8802eaab61dc6fbb205fcd23b19
Alkiros <guillaumetran97@gmail.com>
2015-09-04 14:42:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<head>
+	<script src="ajax/js/jquery-1.11.3.min.js"></script>
+	<script src="ajax/js/main.js"></script>
+	<link rel="stylesheet" href="ajax/css/style.css">
+</head>
+<header><div><h1>.</h1></div><div><h1>.</h1></div><div><h1>.</h1></div></header>
+	<body>
+
+<center>
+<div id="jeu">
+
+		<?php include 'ajax/ajax.php'; ?>
+</div>
+<button><a href="index.php">Rejouer<a></button>
+	</center>
+</body>
